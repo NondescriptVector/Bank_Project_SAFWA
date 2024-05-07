c6d823d5452119a708fb596cb6d86fa581c26f75
Ahmed Ibrahim Ayad <160779925+Ahmed404error@users.noreply.github.com>
2024-05-07 12:28:52 +0300

    Update Manager.java

--- src/Manager.java
+++ src/Manager.java
@@ -1,11 +1,11 @@
+import javax.swing.JOptionPane;
 import java.util.*;
 
-public class Manager extends Person{
+public class Manager extends Person {
     double salary = 3100;
-    static Scanner scanner= new Scanner(System.in);
-    //attributes
-    ArrayList<Customer> customers=new ArrayList<>();
-    ArrayList<Employee>employees=new ArrayList<>();
+    /
+    ArrayList<Customer> customers = new ArrayList<>();
+    ArrayList<Employee> employees = new ArrayList<>();
 
     public Manager(String name, String address, String username, String password, String id) {
         super(name, address, username, password, id);
@@ -15,53 +15,45 @@ public class Manager extends Person{
         return employees;
     }
 
-    public void changeEmployeePassword(Employee e){
-
-        System.out.println("Enter the new password for the desired employee");
-        String newPassword=scanner.next();
+    public void changeEmployeePassword(Employee e) {
+        String newPassword = JOptionPane.showInputDialog(null, "Enter the new password for the desired employee");
         e.setPassword(newPassword);
-
     }
 
-    public void addEmployee(){
-        System.out.println("enter the employee's name");
-        String name= scanner.next();
-        System.out.println("Enter their address");
-        String address=scanner.next();
-        System.out.println("Create a username for them");
-        String username=scanner.next();
-        System.out.println("Create their password");
-        String password= scanner.next();
-        System.out.println("Enter their id ");
-        String id=scanner.next();
+    public void addEmployee() {
+        String name = JOptionPane.showInputDialog(null, "Enter the employee's name");
+        String address = JOptionPane.showInputDialog(null, "Enter their address");
+        String username = JOptionPane.showInputDialog(null, "Create a username for them");
+        String password = JOptionPane.showInputDialog(null, "Create their password");
+        String id = JOptionPane.showInputDialog(null, "Enter their id");
 
-        employees.add(new Employee(name,address,username,password,id));
-
-
+        employees.add(new Employee(name, address, username, password, id));
     }
-    public void removeEmployee(){
-        System.out.println("here are the Employees, please choose the one whom you'd like to remove");
-        for (Employee e:employees){
-            System.out.println("Employee with id number "+e.id()+" "+e.name);
+
+    public void removeEmployee() {
+        StringBuilder employeeList = new StringBuilder();
+        for (Employee e : employees) {
+            employeeList.append("Employee with id number ").append(e.id()).append(" ").append(e.name).append("\n");
         }
-        int choice=scanner.nextInt();
-        if(choice<0||choice>employees.size()) {
-            System.out.println("enter a valid number");
-            return;
+        JOptionPane.showMessageDialog(null, employeeList.toString());
+
+           int  choice = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the number of the employee you'd like to remove"));
+           int choice1=choice-1;
+        if(choice1 < 0 || choice1>= employees.size()) {
+                JOptionPane.showMessageDialog(null, "Please enter a valid number");}
+        else{
+            
+        JOptionPane.showMessageDialog(null, "Employee removed");
+        employees.removeIf(x -> x.id().equals(String.valueOf(choice1)));
         }
-        System.out.println("Employee removed");
-        employees.removeIf(x->x.id().equals(String.valueOf(choice)));
-
+    
     }
-    public void getEmployeeRecords(Employee employee){
 
-
-        System.out.println("Employee "+employee.name+" has served "+employee.servedCustomers+" customers");
-
-
+    public void getEmployeeRecords(Employee employee) {
+        JOptionPane.showMessageDialog(null, "Employee " + employee.name + " has served " + employee.servedCustomers + " customers");
     }
-    public void adddefaultEmployee(Employee employee){
+
+    public void adddefaultEmployee(Employee employee) {
         employees.add(employee);
-
     }
 }
