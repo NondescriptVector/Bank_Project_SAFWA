2f9a28a109d6628d867abbc1243ada99213047a8
Ahmed Ibrahim Ayad <160779925+Ahmed404error@users.noreply.github.com>
2024-05-07 12:28:17 +0300

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,13 +1,15 @@
+import javax.swing.JOptionPane;
 import java.util.*;
+
 public class Employee extends Person {
-    Scanner scan = new Scanner(System.in);
+   
+    private double salary = 3000.0;
     
-    private double salary=3000.0;
-    
-    ArrayList <Customer> assignedCustomers = new ArrayList<>();
+    LinkedList<Customer> assignedCustomers = new LinkedList<>();
     int servedCustomers = 0;
     
     Customer currentCustomer;
+    
     public Employee(String name, String address, String user_name, String password, String id) {
         super(name, address, user_name, password, id);
     }
@@ -16,53 +18,44 @@ public class Employee extends Person {
         boolean flag = true;
         
         while(flag){
-           
-        System.out.println("To change your password , please enter your old password: ");
-        String oldPass = scan.next();
-        if(this.password()==oldPass) {
-        this.setPassword(pass);
-            System.out.println("Password changed successfully , your new password is: "+pass);
-            flag = false;
+            String oldPass = JOptionPane.showInputDialog(null, "To change your password, please enter your old password:");
+            if(this.password().equals(oldPass)) {
+                this.setPassword(pass);
+                JOptionPane.showMessageDialog(null, "Password changed successfully, your new password is: " + pass);
+                flag = false;
+            } else {
+                JOptionPane.showMessageDialog(null, "Wrong password");
+            }
         }
-        else {
-            System.out.println("Wrong password");
-        }
-            
-        }
-        
     }
+    
     public String currentCustomer(){
-        return currentCustomer+"\nTransaction: "+currentCustomer.wantedTransaction;
+        return currentCustomer + "\nTransaction: " + currentCustomer.wantedTransaction;
     }
     
     public void viewAssignedCustomers() {
-        String ans;
-        
-
         if(currentCustomer != null){
-            System.out.println("Do you want any other transactions?");
-            ans = scan.next();
-            if(ans.equalsIgnoreCase("no")) {
+            int ans = JOptionPane.showConfirmDialog(null, "Do you want any other transactions?", "Transaction", JOptionPane.YES_NO_OPTION);
+            if(ans == JOptionPane.NO_OPTION) {
                 servedCustomers++;
                 currentCustomer = null;
+            } else {
+                return;
             }
-            else return;
         }
 
         if (assignedCustomers.isEmpty()) {
-            System.out.println("No assigned Customers yet");
+            JOptionPane.showMessageDialog(null, "No assigned Customers yet");
             return;
         }
+        
         for (Customer customer : assignedCustomers) {
-            System.out.println(customer);
+            JOptionPane.showMessageDialog(null, customer.toString());
         }
 
-        System.out.println("Do you want to proceed to the next customer?");
-        ans = scan.next();
-        if(ans .equalsIgnoreCase("yes"))  {
-            currentCustomer = assignedCustomers.getFirst();
-
-            assignedCustomers.removeFirst();
+        int proceed = JOptionPane.showConfirmDialog(null, "Do you want to proceed to the next customer?", "Next Customer", JOptionPane.YES_NO_OPTION);
+        if(proceed == JOptionPane.YES_OPTION) {
+            currentCustomer = assignedCustomers.poll();
         }
     }
 
